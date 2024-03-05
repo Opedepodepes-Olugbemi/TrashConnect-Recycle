122a0b59eec119f9b1b2b4f3277f003a766e1a5c
olugbemiopedepo <olugbemiopedepo@gmail.com>
2024-03-05 08:01:07 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,89 @@
 import 'package:flame/components.dart';
-import 'package:flutter/material.dart';
+import 'package:flame/events.dart';
+import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    GameWidget(
-      game: TrashConnect(),
-    ),
-  );
+  runApp(const GameWidget.controlled(gameFactory: TrashConnect.new));
 }
 
 class TrashConnect extends Forge2DGame {
-  TrashConnect() : super(gravity: Vector2(0, 10.0));
+  TrashConnect() : super(gravity: Vector2(0, 5.0));
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    Vector2 gameSize = screenToWorld(camera.viewport.effectiveSize);
-    add(Ground(gameSize));
-    add(Player());
+
+    camera.viewport.add(FpsTextComponent());
+    world.add(Player());
+    world.addAll(createBoundaries());
+  }
+
+  List<Component> createBoundaries() {
+    final visibleRect = camera.visibleWorldRect;
+    final topLeft = visibleRect.topLeft.toVector2();
+    final topRight = visibleRect.topRight.toVector2();
+    final bottomRight = visibleRect.bottomRight.toVector2();
+    final bottomLeft = visibleRect.bottomLeft.toVector2();
+
+    return [
+      Wall(topLeft, topRight),
+      Wall(topRight, bottomRight),
+      Wall(bottomLeft, bottomRight),
+      Wall(topLeft, bottomLeft),
+    ];
   }
 }
 
-class Player extends BodyComponent {
+class Player extends BodyComponent with TapCallbacks {
+  Player({Vector2? initialPosition})
+      : super(
+          fixtureDefs: [
+            FixtureDef(
+              CircleShape()..radius = 2,
+              restitution: 0.4,
+              friction: 0.4,
+            ),
+          ],
+          bodyDef: BodyDef(
+            angularDamping: 0.8,
+            position: initialPosition ?? Vector2.zero(),
+            type: BodyType.dynamic,
+          ),
+        );
+
+  @override
+  void onTapDown(event) {
+    body.applyLinearImpulse(Vector2.random() * 5000);
+  }
+
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     renderBody = false;
-    add(
-      SpriteComponent(
-        sprite: await game.loadSprite('trash.png'),
-        anchor: Anchor.center,
-      ),
-    );
-  }
 
-  @override
-  Body createBody() {
-    final shape = CircleShape()..radius = 10;
-    final fixtureDef = FixtureDef(shape, friction: 0.5);
-    final bodyDef = BodyDef(position: Vector2(300, 20), type: BodyType.dynamic);
-    return world.createBody(bodyDef)..createFixture(fixtureDef);
+
+    add(SpriteComponent(
+        sprite: await game.loadSprite('trash.png'), size: Vector2(2,10), anchor: Anchor.center));
   }
 }
 
-class Ground extends BodyComponent {
-  final Vector2 gameSize;
+class Wall extends BodyComponent {
+  final Vector2 _start;
+  final Vector2 _end;
 
-  Ground(this.gameSize);
+  Wall(this._start, this._end);
 
   @override
   Body createBody() {
-    final shape = EdgeShape()
-      ..set(Vector2(0, gameSize.y - 3), Vector2(gameSize.x, gameSize.y - 3));
-    final fixtureDef = FixtureDef(shape, friction: 0.5);
-    final bodyDef = BodyDef(userData: this, position: Vector2.zero());
+    final shape = EdgeShape()..set(_start, _end);
+    final fixtureDef = FixtureDef(shape, friction: 0.3);
+    final bodyDef = BodyDef(
+      position: Vector2.zero(),
+    );
+
     return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 }
